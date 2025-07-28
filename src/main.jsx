327fabf74fbf808b090ddcdf6955dcded4f157c7
Ritik2317 <riteeky@gmail.com>
2025-07-28 23:15:57 +0530

    Added Routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import Income from './pages/Dashboard/Income';
+import Home from './pages/Dashboard/Home';
+import Expense from './pages/Dashboard/Expense';
+import Signup from './pages/Auth/Signup';
+import Login from './pages/Auth/Login';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/income',
+    element: <Income />,
+  },
+  {
+    path: '/home',
+    element: <Home />,
+  },
+  {
+    path: '/expense',
+    element: <Expense />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
+
